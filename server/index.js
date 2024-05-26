54a582e17605c2eda8003e2b55286d7933739512
Kelly Hum <kellyhum88@gmail.com>
2024-05-25 23:59:09 -0700

    pull data from file

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 // code from https://www.freecodecamp.org/news/how-to-create-a-react-app-with-a-node-backend-the-complete-guide/
 // server/index.js
-
+const shelf1 = require("../client/src/shelf1.json");
 const express = require("express");
+const app = express(); // start app
+const PORT = 3001;
 
-const PORT = process.env.PORT || 3001;
-
-const app = express();
+let initialData = shelf1.data;
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: `${initialData.map((book) => book.author)}` });
 });
 
+// start server
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
